b0df273830d9f9cfb5513cc51a9a1f54dcc5d7c3
Wayne Cheng at ChihLei's Asus <waynewccheng@gmail.com>
2022-05-28 00:04:03 -0400

    0528

--- @cs2000Class/cs2000Class.m
+++ @cs2000Class/cs2000Class.m
@@ -40,6 +40,48 @@ classdef cs2000Class < handle
             % close RS232
             obj.comPort.close;
         end
+
+        function Yxy = measure_Yxy (obj)
+
+            %% --- trigger measurement
+            obj.comPort.send('MEAS,1');
+
+            % pre-measurement
+            err = obj.comPort.get;
+            if (err(1:5)=='OK00,')
+                wait = err(6:length(err));
+                waitsec = str2num(wait) + 1;
+                pause(waitsec)
+            else
+                disp error pre-measure
+                obj.comPort.close;
+                obj.close;
+            end
+
+            % actual measurement completed
+            err = obj.comPort.get;
+            if (err(1:4)~='OK00')
+                disp error actual
+                obj.comPort.close;
+                obj.close;
+            end
+
+            %% --- obtain colorimetric measurement data
+            obj.comPort.send('MEDR,2,0,02');
+            err = obj.comPort.get;
+            if (err(1:4) ~= 'OK00')
+                disp error obtain
+                obj.comPort.close;
+                obj.close;
+            else
+                % parse result
+                val = textscan(err,'%s %f %f %f\n','Delimiter',',');
+                vciex = cell2mat(val(2));
+                vciey = cell2mat(val(3));
+                vy = cell2mat(val(4));
+                Yxy = [vy vciex vciey atan2d(vciey,vciex) (vciey.^2+vciex.^2).^0.5];
+            end            
+        end
         
         %% Take a measurement
         function myspec = measure (obj)
@@ -85,7 +127,7 @@ classdef cs2000Class < handle
                 Yxy = [vy vciex vciey];
                 
                 % show the Yxy?
-                % Yxy
+                Yxy
                 %
             end
 
