f99017edc75b5c06a5446b04c2624625bfd841b7
waynewccheng <waynewccheng@gmail.com>
2022-08-23 11:11:58 -0400

    Update mychromaticity.m

--- mychromaticity.m
+++ mychromaticity.m
@@ -1,29 +1,40 @@
-[X Y] = meshgrid(0:0.01:1,0:0.01:1);
+% create chromaticity plane
+step = 1/200;
+[X Y] = meshgrid(0:step:1,0:step:1);
 Z = 1 - X - Y;
 
+% linearize
 X1 = X(:);
 Y1 = Y(:);
 Z1 = Z(:);
 mask0 = (Z1 < 0) | (Z1 > 1);
 
-% targetY = 0.5 / (X1 + Y1 + Z1);
-% 
-% X1 = X1 ./ Y1 .* targetY;
-% Y1 = Y1 ./ Y1 .* targetY;
-% Z1 = Z1 ./ Y1 .* targetY;
+xyz_blue = rgb2xyz([0 0 1])
+targetY = xyz_blue(2)*10
+
+X1 = X1 ./ Y1 .* targetY;
+Y1 = Y1 ./ Y1 .* targetY;
+Z1 = Z1 ./ Y1 .* targetY;
 
 XYZ1 = [X1 Y1 Z1];
 mask = (Z1 < 0) | (Z1 > 1);
 
-
 rgb1 = xyz2rgb(XYZ1);
+rgb1(mask,:) = 0;
+
 rgb1my = rgb1;
+rgb2 = reshape(rgb1,size(X,1),size(X,2),3);
+imshow(rgb2)
+axis equal
+
+size(rgb2)
+%return
 
 %rgb1(mask0,:) = 0;
 
 mask1 = (rgb1(:,1) < 0);
 mask11 = (rgb1(:,1) > 1);
-mask111 = (rgb1(:,1) <1.6);
+%mask111 = (rgb1(:,1) <1.6);
 
 mask2 = (rgb1(:,2) < 0);
 mask22 = (rgb1(:,2) > 1);
@@ -31,7 +42,7 @@ mask3 = (rgb1(:,3) < 0);
 mask33= (rgb1(:,3) > 1);
 
 rgb1(mask1,:) = 0;
-%rgb1(mask11,:) = 0;
+rgb1(mask11,:) = 0;
 rgb1(mask2,:) = 0;
 rgb1(mask22,:) = 0;
 rgb1(mask3,:) = 0;
